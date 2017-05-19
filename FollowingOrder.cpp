57eb5946a5131d2470223ecfd3d329edd2a68560
abietic <iamyhc@live.cn>
2017-05-19 19:30:40 +0800

    Update FollowingOrder.cpp

--- FollowingOrder.cpp
+++ FollowingOrder.cpp
@@ -28,7 +28,7 @@ int main()
     char str[1000];
     while (cin.getline(str , 1000 , '\n'))
     {
-        node aovnet[26];
+        node aovnet[26];//通过直接字母序存储节点省去了排序。很巧妙。
         char from , to;
         int len = 0;
         for (int i = 0 ; str[i] != '\0' ; i++)
@@ -40,7 +40,7 @@ int main()
             }
         }
         cin.getline(str , 1000 , '\n');
-        for (int i = 0 , j = strlen(str) ; i < j ; i += 4)
+        for (int i = 0 , j = strlen(str) ; i < j ; i += 4)//读取关系的方式很巧妙，完全避免了题目模棱两可的输入标准带来的错误。之前疯狂RE
         {
             from = str[i];
             to = str[i + 2];
@@ -58,7 +58,7 @@ void dfs(node aovnet[] , int total , int len)
 {
     if (total == len)
     {
-        output[total] = '\0';
+        output[total] = '\0';//这里len是个数，而total则是数组下一个录入的位置。原来写total+1疯狂WA
         cout << output << endl;
         return;
     }
@@ -75,6 +75,7 @@ void dfs(node aovnet[] , int total , int len)
         }
         output[total] = 'a' + i;
         dfs(aovnet , total + 1 , len);
+        //进行回溯，这里是DFS的关键。
         for (int j = 0 ; j < aovnet[i].adj.size() ; j++)
         {
             aovnet[aovnet[i].adj[j]].degree++;
